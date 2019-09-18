a233f6cfdbfcb2f78afa29b894b5cb28f7400e4d
Armin <e11771237@student.tuwien.ac.at>
2019-09-18 15:09:10 +0200

    Update
    Finished Grid 2 Random Computer and started AI Computer in Grid 3

--- src/components/Grid3.js
+++ src/components/Grid3.js
@@ -28,7 +28,115 @@ export default () => {
   let aiPlayer = "O";
   // draw
   let draw = "D";
- 
+  /*
+  //keeps count of function calls
+  var fc = 0;
+  
+  // finding the ultimate play on the game that favors the computer
+  var bestSpot = minimax(game, aiPlayer);
+  
+  //loging the results
+  console.log("index: " + bestSpot.index);
+  console.log("function calls: " + fc);
+  
+  // the main minimax function
+  function minimax(game, player){
+    //add one to function calls
+    fc++;
+    
+    //available spots
+    var availSpots = emptyIndexies(game);
+  
+    // checks for the terminal states such as win, lose, and tie and returning a value accordingly
+    if (winning(game, huPlayer)){
+       return {score:-10};
+    }
+    else if (winning(game, aiPlayer)){
+      return {score:10};
+    }
+    else if (availSpots.length === 0){
+      return {score:0};
+    }
+  
+  // an array to collect all the objects
+    var moves = [];
+  
+    // loop through available spots
+    for (var i = 0; i < availSpots.length; i++){
+      //create an object for each and store the index of that spot that was stored as a number in the object's index key
+      var move = {};
+      move.index = game[availSpots[i]];
+  
+      // set the empty spot to the current player
+      game[availSpots[i]] = player;
+  
+      //if collect the score resulted from calling minimax on the opponent of the current player
+      if (player === aiPlayer){
+        var result = minimax(game, huPlayer);
+        move.score = result.score;
+      }
+      else{
+        var result = minimax(game, aiPlayer);
+        move.score = result.score;
+      }
+  
+      //reset the spot to empty
+      game[availSpots[i]] = move.index;
+  
+      // push the object to the array
+      moves.push(move);
+    }
+  
+  // if it is the computer's turn loop over the moves and choose the move with the highest score
+    var bestMove;
+    if(player === aiPlayer){
+      var bestScore = -10000;
+      for(var i = 0; i < moves.length; i++){
+        if(moves[i].score > bestScore){
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    }else{
+  
+  // else loop over the moves and choose the move with the lowest score
+      var bestScore = 10000;
+      for(var i = 0; i < moves.length; i++){
+        if(moves[i].score < bestScore){
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    }
+  
+  // return the chosen move (object) from the array to the higher depth
+    return moves[bestMove];
+  }
+  
+  // returns the available spots on the game
+  function emptyIndexies(game){
+    return  game.filter(s => s !== "O" && s !== "X");
+  }
+  
+  // winning combinations using the game indexies for instace the first win could be 3 xes in a row
+  function winning(game, player){
+   if (
+          (game[0] === player && game[1] === player && game[2] === player) ||
+          (game[3] === player && game[4] === player && game[5] === player) ||
+          (game[6] === player && game[7] === player && game[8] === player) ||
+          (game[0] === player && game[3] === player && game[6] === player) ||
+          (game[1] === player && game[4] === player && game[7] === player) ||
+          (game[2] === player && game[5] === player && game[8] === player) ||
+          (game[0] === player && game[4] === player && game[8] === player) ||
+          (game[2] === player && game[4] === player && game[6] === player)
+          ) {
+          return true;
+      } else {
+          return false;
+      }
+  }
+  */
+
   function pcMove(currentState) {
     const emptyFields = currentState.reduce((acc, val, i) => {
       if (val === null) {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Grid from "./components/Grid";
 import Grid2 from "./components/Grid2";
+import Grid3 from "./components/Grid3";
 
 import "./styles.css";
 
@@ -11,8 +12,11 @@ function App() {
       <h1>Tic-Tac-Toe Player vs Player</h1>
       <Grid/>
     
-      <h1>Tic-Tac-Toe Computer vs Player</h1>
+      <h1>Tic-Tac-Toe Random Computer vs Player</h1>
       <Grid2/>
+
+      <h1>Tic-Tac-Toe AI Computer vs Player</h1>
+      <Grid3/>
     </div>
   );
 }
